7739a1d8c64688fd13557773e193f645c1beeb7f
td2thinh <75013885+td2thinh@users.noreply.github.com>
2021-11-10 10:03:57 +0700

    main file in progress

--- main.cpp
+++ main.cpp
@@ -6,11 +6,28 @@ using namespace std;
 
 int main()
 {
+    int dataSize[5] = {10000, 50000, 100000, 300000, 500000};
     int compare = 0;
-    int n = 10000;
-    int a[n];
-    GenerateRandomData(a, n);
-    MergeSort_Compare(a, n, 0, n - 1, compare);
-    cout << compare;
+
+    cout << "Time: " << MergeSort_Timing(a, n);
+    for (int i = 0; i < 5; i++)
+    {
+        n = a[i];
+        int *arr = new int[n];
+
+        GenerateData(arr, n, 0);
+
+        ShellSort_Timing(arr, n);
+        RadixSort_Timing(arr, n);
+        ShakerSort_Timing(arr, n);
+        SelectionSort_Timing(arr, n);
+        InsertionSort_Timing(arr, n);
+        HeapSort_Timing(arr, n);
+        BubbleSort_Timing(arr, n);
+        QuickSort_Timing(arr, n);
+        MergeSort_Timing(arr, n);
+        CountingSort_Timing(arr, n);
+        FlashSort_Timing(arr, n);
+    }
     return 0;
 }
