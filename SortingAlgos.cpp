ee2ca8fd508ba8d88d8be6e661b8096fd668f03f
td2thinh <75013885+td2thinh@users.noreply.github.com>
2021-11-10 09:21:39 +0700

    All in one file

--- SortingAlgos.cpp
+++ SortingAlgos.cpp
@@ -1,4 +1,4 @@
-#include "header.h"
+#include "functions.h"
 #include <iostream>
 using namespace std;
 //Shell sort----------------------------------------------------------------------------------------------
