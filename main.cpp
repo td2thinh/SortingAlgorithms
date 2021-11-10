c25134c454fd5621bc07b970cc17add564a1699b
td2thinh <75013885+td2thinh@users.noreply.github.com>
2021-11-10 15:51:16 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,52 +8,111 @@ using namespace std;
 
 int main()
 {
-    //ofstream file("Radix Timing.txt");
+    ofstream file("Random Timing.txt");
 
     int dataSize[5] = {10000, 50000, 100000, 300000, 500000};
     string dataOrder[4] = {"Random", "Sorted", "Reverse", "Nearly Sorted"};
+    int i = 0;
+    file << dataOrder[i] << endl;
 
-    /*    for (int i = 0; i < 4; i++)
+    for (int j = 0; j < 5; j++)
     {
-        file << dataOrder[i] << endl;
+        long long compare;
+        int n = dataSize[j];
+        int *arr = new int[n];
+        file << "Data Size: " << n << "\n\n";
+        GenerateData(arr, n, i);
 
-        for (int j = 0; j < 5; j++)
-        {
-            long long compare = 0;
-            int n = dataSize[j];
-            int *arr = new int[n];
-            file << "Data Size: " << n << "\n\n";
-            GenerateData(arr, n, i);
-
-            //file << "Shell Sort: " << ShellSort_Timing(arr, n) << endl;
-            //ShellSort_Comparison(arr, n, compare);
-
-            file << "Radix Short: " << RadixSort_Timing(arr, n) << endl;
-            RadixSort_Comparison(arr, n, compare);
-            //file << "Shaker Short: " << ShakerSort_Timing(arr, n) << endl;
-            //file << "Selection Short: " << SelectionSort_Timing(arr, n) << endl;
-            //file << "Insertion Short: " << InsertionSort_Timing(arr, n) << endl;
-            //file << "Heap Short: " << HeapSort_Timing(arr, n) << endl;
-            //HeapSort_Compare(arr, n, compare);
-            //file << "Bubble Short: " << BubbleSort_Timing(arr, n) << endl;
-            //file << "Quick Short: " << QuickSort_Timing(arr, n) << endl;
-            //QuickSort_Compare(arr, 0, n - 1, compare);
-            //file << "Merge Short: " << MergeSort_Timing(arr, n) << endl;
-            //MergeSort_Compare(arr, n, 0, n - 1, compare);
-            //file << "Couting Short: " << CountingSort_Timing(arr, n) << endl;
-            //CountingSort_Comparison(arr, n, compare);
-
-            //file << "Flash Short: " << FlashSort_Timing(arr, n) << endl;
-            //FlashSort_Compare(arr, n, compare);
-            file
-                << "Comparisons: " << compare << endl;
-
-            delete[] arr;
-        }
+        file << "Shell Sort: " << ShellSort_Timing(arr, n) << endl;
+        ShellSort_Comparison(arr, n, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Radix sort: " << RadixSort_Timing(arr, n) << endl;
+        RadixSort_Comparison(arr, n, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Shaker sort: " << ShakerSort_Timing(arr, n) << endl;
+        ShakerSort_Compare(arr, n, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Selection sort: " << SelectionSort_Timing(arr, n) << endl;
+        SelectionSort_Compare(arr, n, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Insertion sort: " << InsertionSort_Timing(arr, n) << endl;
+        InsertionSort_Compare(arr, n, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Heap sort: " << HeapSort_Timing(arr, n) << endl;
+        HeapSort_Compare(arr, n, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Bubble sort: " << BubbleSort_Timing(arr, n) << endl;
+        BubbleSort_Compare(arr, n, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Quick sort: " << QuickSort_Timing(arr, n) << endl;
+        QuickSort_Compare(arr, 0, n - 1, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Merge sort: " << MergeSort_Timing(arr, n) << endl;
+        MergeSort_Compare(arr, n, 0, n - 1, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Couting sort: " << CountingSort_Timing(arr, n) << endl;
+        CountingSort_Comparison(arr, n, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
+        arr = new int[n];
+        compare = 0;
+        GenerateData(arr, n, i);
+        file << "Flash sort: " << FlashSort_Timing(arr, n) << endl;
+        FlashSort_Compare(arr, n, compare);
+        file
+            << "Comparisons: " << compare << endl;
+        delete[] arr;
 
         file << "\n\n";
     }
-    */
+
+    /*
     int n = 300000;
 
     long long compare;
@@ -76,7 +135,7 @@ int main()
     MergeSort_Compare(a, n, 0, n - 1, compare);
     cout << compare << endl;
     delete[] a;
-
+*/
     cout << "done";
     return 0;
 }
